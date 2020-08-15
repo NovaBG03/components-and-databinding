9be24e20a11d3aecd8125982bbc89a61d372110d
Nikita Koynov <nikicha03@gmail.com>
2020-08-15 21:55:43 +0300

    using @ContentChild

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,16 +1,21 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {Component, OnInit, Input, ContentChild, ElementRef, AfterContentInit} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, AfterContentInit {
   @Input('srvElement') element: {name: string, type: string, content: string};
+  @ContentChild('paragraphElement', {static: true}) paragraph: ElementRef;
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log(`OnInit ContentChild: ${this.paragraph.nativeElement.textContent}`)
   }
 
+  ngAfterContentInit() {
+    console.log(`AfterContentInit ContentChild: ${this.paragraph.nativeElement.textContent}`)
+  }
 }
